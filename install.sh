ccbc3586a9a49c7b1f8087ca381415a29d54eadd
Mohit <mohitsaraswat411@gmail.com>
2020-08-03 13:08:47 +0530

    updated

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+while true; do
+    read -p "Do you wish to install this program?" yn
+    read -p "press y to install " yn
+    read -p "press n to cancle install " yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 cp -r ~/termux-startup-tone/bash.bashrc /data/data/com.termux/files/usr/etc/
 cd ~/
 mkdir .tone
@@ -5,9 +15,9 @@ cp -r ~/termux-startup-tone/startup-sound.mp3 ~/.tone/
 date | lolcat
 pkg install screenfetch
 date | lolcat
-pkg install mpv -y
+pkg install mpv
 date | lolcat
-pkg install pulseaudio -y
+pkg install pulseaudio
 clear
 cd ..
 cd .. 
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,13 @@
+while true; do
+    read -p "Do you wish to uninstall this program?" yn
+    read -p "press y to uninstall" yn
+    read -p "press n to cancle uninstall" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 cd
 cp -r ~/termux-startup-tone/bash.bash /data/data/com.termux/files/usr/etc/
 cd /data/data/com.termux/files/usr/etc/
@@ -6,9 +16,9 @@ mv bash.bash bash.bashrc
 cd ~/
 rm -rf .tone
 date
-pkg uninstall mpv -y
+pkg uninstall mpv
 date
-pkg uninstall pulseaudio -y
+pkg uninstall pulseaudio
 cd
 rm -rf termux-startup-tone
 cd
